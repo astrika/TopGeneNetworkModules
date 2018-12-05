61b2f48a6119562e8f4775587e299da2c86b986d
astrika <astrika.0914@gmail.com>
2018-12-04 18:01:38 -0600

    added line to make GSEA input files

--- obtainTopNetworkModules.R
+++ obtainTopNetworkModules.R
@@ -1,8 +1,9 @@
 # Astrid M Manuel 11/27/2018
 # Top 1% network modules from dmGWAS results are examined for the highest association scores
 
+study <- "2009o"
 # set your node-weighted network to res.list and obtain all modules
-load("C:\\users\\amanuel1\\Dev\\ZhaoLab\\MS\\Results\\RESULT.list2009o.RData")
+load(paste("C:\\users\\amanuel1\\Dev\\ZhaoLab\\MS\\Results\\RESULT.list", study, ".RData", sep=""))
 allModules <- res.list$genesets.clear
 total <- length(allModules)
 
@@ -12,13 +13,16 @@ top1percent <- ceiling(length(allModules)*0.01)
 # get your top 1% modules
 top1percentModules <- head(res.list$zi.ordered, top1percent)
 
+# get gene set enrichment analysis input
+write.table(top1percentModules[,c(1,3)], file = paste("Zn", study,".txt",sep=""), append = T, quote = F, row.names = F, col.names = F, sep = "\t")
+
 # get the seed genes from your top 1% modules for module identifications
 top1percentSeedGenes <- top1percentModules$gene
 
 
 #obtain file of top modules content and all of the non redundant genes
 for(gene in top1percentSeedGenes){
-  write.table(print(getElement(allModules,gene)), file = "topModulesContent2009o.txt", append = T, col.names = F, quote = F)
+  write.table(print(getElement(allModules,gene)), file = paste("topModulesContent", study,".txt", sep=""), append = T, col.names = F, quote = F)
 }
 
 #print a message for summary
